e206b3045b31bc8687a47f34b6fb92829c8d2123
Your Name <you@example.com>
2024-07-22 12:36:28 +0000

    format of print

--- printf_format.c
+++ printf_format.c
@@ -1,3 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+int _print_format(char specifier, va_list ap)
+{
+	int count;
+
+	if (specifier == 'c')
+		count += _print_char(va_arg(ap, int));
+	else if (specifier == 's')
+		count += _print_string(va_arg(ap, char*));
+	else if (specifier == 'd')
+		count += _print_integer(va_arg(ap, unsigned int));
+	else if (specifier == '%')
+		count += _print_float(va_arg(ap, float));
+	else if (specifier == 'i')
+		count += _print_integer(va_arg(ap, int));
+	else
+		count += write(1, &specifier, 1);
+
+	return count;
+}
