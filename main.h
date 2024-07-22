d76b09114bb1a37c483b89bd0ca8dd674bd83f6c
Your Name <you@example.com>
2024-07-22 21:00:25 +0000

    add prototype in the librairy

--- main.h
+++ main.h
@@ -1,24 +1,32 @@
-#ifndef MAIN_H 
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <unistd.h>
-#include <stdarg.h>
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
+#include <stdarg.h>
+
+/**
+ * struct format_sp - Struct for format specifiers and
+ * their corresponding functions
+ * @specifi: The format specifier character
+ * ('c' for char, 's' for string)
+ * @print_func: Function pointer to
+ * the function that prints the corresponding type
+ */
 
 typedef struct format_sp
 {
-		char specifi;
-			void (*print_func)(va_list ap);
+	char specifi;
+	int (*print_func)(va_list args);
 } sp_t;
 
-void print_char(va_list ap);
-void print_float(va_list ap);
+int print_char(va_list args);
 void print_integer(va_list ap);
-void print_string(va_list ap);
-int _printf(const char *format, ...);
-int _print_format(char specifier, va_list ap);
+int print_string(va_list args);
+int printf(const char *format, ...);
+int print_percent(va_list args);
 int _putchar(char c);
-
+int _printf(const char *format, ...);
 #endif
