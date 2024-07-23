b15b73a1b4da837bd74630c9f0e271562d82d24e
Your Name <you@example.com>
2024-07-23 13:29:08 +0000

    problem to return

--- _string.c
+++ _string.c
@@ -12,10 +12,9 @@ int print_string(va_list args)
 {
 	int count = 0;
 	char *s = va_arg(args, char *);
-
+	
 	if (s == NULL)
-		s = (NULL);
-
+		s = "(null)";
 	while (s[count])
 	{
 		_putchar(s[count++]);
